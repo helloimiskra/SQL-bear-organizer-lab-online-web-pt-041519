e8c6beb842528ed1dc13733d24cc8e81c2d4a5b3
Iskra B <ibatistapoblete@gmail.com>
2019-07-01 03:06:36 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (
+  (1, 'Mr. Chocolate', 4, "male", "brown", "friendly", 0),
+  (2, 'Rowdy', 2, "male", "brown", "friendly", 0),   
+  (3, 'Tabitha', 5, "female", "brown", "friendly", 0),
+  (4, 'Sergeant Brown', 1, "male", "brown", "friendly", 0),
+  (5, 'Melissa', 6, "female", "brown", "friendly", 0),
+  (6, 'Grinch', 3, "male", "brown", "friendly", 0),
+  (7, 'Wendy', 10, "female", "brown", "friendly", 0),
+  (8, null, 4, "male", "brown", "friendly", 0)
+);
