7bd9977c9f0b64937bb641baa2d6fdb512db5262
Iskra B <ibatistapoblete@gmail.com>
2019-07-01 03:36:43 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -4,6 +4,7 @@ end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
   "SELECT name FROM bears ORDER BY name DESC;"
+end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
   "Write your SQL query here"
